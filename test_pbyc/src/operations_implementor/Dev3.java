77ea2d17c973edfa52d9aa26708b230384eb8e36
noopur1004 <noopur.mohite1004@gmail.com>
2025-06-05 19:51:54 +0530

    Delete test_pbyc directory

--- test_pbyc/src/operations_implementor/Dev3.java
+++
@@ -1,9 +0,0 @@
-package operations_implementor;
-
-public class Dev3 extends Tl{
-	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
-		System.out.println("dev3 logic");
-	}
-}
--- test_pbyc/src/operations_implementor/operations_implementor.java
+++
@@ -1,7 +0,0 @@
-package operations_implementor;
-
-import operations.operations;
-
-abstract public class operations_implementor implements operations{
-
-}
